c3ca484f1149ea99e5e60464c5006cb2b9292c5e
emsbenson <86622219+emsbenson@users.noreply.github.com>
2021-07-16 13:29:39 -0400

    Update sapflow.R

--- sapflow.R
+++ sapflow.R
@@ -1,9 +1,9 @@
 #read data
 library(lubridate)
 library(ggplot2)
-datsap<- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\07_09_2021\\Sapflow_TableDT.dat",
+datsap<- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\07_16_2021\\Sapflow_TableDT.dat",
                   skip = 4, header = FALSE, na.strings = "NAN")
-datNames <- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\07_09_2021\\Sapflow_TableDT.dat",
+datNames <- read.csv("K:\\Environmental_Studies\\hkropp\\Data\\campus\\buckthorn\\sapflux\\campbell\\07_16_2021\\Sapflow_TableDT.dat",
                   skip = 1, nrows = 2, header = FALSE)
 tabledt <- datsap[,1:18]
 colnames(tabledt) <- c("date", "record", paste0("dT",seq(1, 16)))
@@ -12,33 +12,33 @@ plot(tabledt$record, tabledt$dT5, type = "l")
 tabledt$datef <- ymd_hms(tabledt$date)
 ggplot(data = tabledt,aes(datef, dT1)) +
   geom_path()+
-  ggtitle("Sensor 1 7/06/21")
+  ggtitle("Sensor 1 7/16/21")
 ggplot(data = tabledt,aes(datef, dT2)) +
   geom_path() +
-  ggtitle("Sensor 2 7/06/21")
+  ggtitle("Sensor 2 7/16/21")
 ggplot(data = tabledt,aes(datef, dT3)) +
   geom_path()+
-  ggtitle("Sensor 3 7/06/21")
+  ggtitle("Sensor 3 7/16/21")
 ggplot(data = tabledt,aes(datef, dT4)) +
   geom_path()+
-  ggtitle("Sensor 4 7/06/21")
+  ggtitle("Sensor 4 7/16/21")
 ggplot(data = tabledt,aes(datef, dT5)) +
   geom_path()+
-  ggtitle("Sensor 5 7/06/21")
+  ggtitle("Sensor 5 7/16/21")
 ggplot(data = tabledt,aes(datef, dT6)) +
   geom_path()+
-  ggtitle("Sensor 6 7/06/21")
+  ggtitle("Sensor 6 7/16/21")
 ggplot(data = tabledt,aes(datef, dT7)) +
   geom_path()+
-  ggtitle("Sensor 7 7/06/21")
+  ggtitle("Sensor 7 7/16/21")
 ggplot(data = tabledt,aes(datef, dT8)) +
   geom_path()+
-  ggtitle("Sensor 8 7/06/21")
+  ggtitle("Sensor 8 7/16/21")
 
 
 sensors <- read.csv("K://Environmental_Studies/hkropp/Data/campus/buckthorn/sapflux/sensors_meta.csv")
-ash <- sensors[sensors$Type =="Ash", ]
-ash$sd.cm <- -36.33+(44.28*(1-exp(-0.1306*ash$DBH.cm)))
+ash <- sensors[sensors$Type == "Ash", ]
+ash$sd.cm <- -36.33 + (44.28*(1-exp(-0.1306*ash$DBH.cm)))
 
 
 tabledt$doy <- yday(tabledt$date)
@@ -65,7 +65,7 @@ dtAll <- data.frame(date= rep(tabledt$date, times = 16),
                            tabledt[,17],
                            tabledt[,18]))
 
-night <- dtAll[dtAll$hour <5|dtAll$hour>=22,]
+night <- dtAll[dtAll$hour < 5|dtAll$hour >= 22,]
 maxnight <- aggregate(night$dT, by = list(sensor = night$sensor, 
                                           doy = night$doy), 
                                           FUN = "max")
